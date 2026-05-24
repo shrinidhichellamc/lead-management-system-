79ffbb8d91846554da4aadd081b3a91ffa603df7
shrinidhichellamc <shrinidhichellamc@gmail.com>
2026-05-24 19:36:53 +0530

    automattically redirects to homepage

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,8 @@
-export default function Home() {
+import { redirect } from "next/navigation";
 
+
+export default function Home() {
+  redirect("/dashboard");
   return (
 
     <div className="p-10">
